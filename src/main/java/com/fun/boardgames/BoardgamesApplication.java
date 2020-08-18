0fc5ec28ef25d7bc65ed5fecf2ed33a98161243b
Lili Cserhalmi <lili_cserhalmi@epam.com>
2020-08-18 21:23:57 +0200

    Initial commit

---
+++ src/main/java/com/fun/boardgames/BoardgamesApplication.java
@@ -0,0 +1,13 @@
+package com.fun.boardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgamesApplication.class, args);
+	}
+
+}
